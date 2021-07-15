6bffa408004f36719557ae702b62125235b245e5
Maia Salholz-Hillel <mash2015@gmail.com>
2021-07-15 13:32:32 +0200

    Integrate updated intovalue data

--- R/01_get-data.R
+++ R/01_get-data.R
@@ -7,24 +7,13 @@ dir <- dir_create(here::here("data", "raw"))
 
 # INTOVALUE ---------------------------------------------------------------
 
-# intovalue <-
+intovalue <- read_csv("https://github.com/maia-sh/intovalue-data/raw/master/data/processed/intovalue.csv")
+# intovalue <- rio::import("https://github.com/maia-sh/intovalue-data/raw/master/data/processed/intovalue.rds")
 
-#TODO: add path to intovalue_data
-# ead_csv(""))
-
-# write_csv(intovalue, path(dir, "intovalue.csv"))
-
+write_csv(intovalue, path(dir, "intovalue.csv"))
 
 # CROSS-REGISTRATIONS -----------------------------------------------------
 
-# Currently rely on reading data from private repo on MSH computer which will make public later and change the line
-# Get data from other repositories, assuming both repositories in same parent directory
-
-dir_repositories <- path_norm(path_wd(".."))
-dir_repository <- path(dir_repositories, "reg-pub-link")
-
-intovalue_crossreg <-
-
-  read_rds(path(dir_repository, "data", "processed", "cross-registrations.rds"))
+intovalue_crossreg <- read_csv("https://github.com/maia-sh/intovalue-data/raw/master/data/raw/cross-registrations.csv")
 
 write_csv(intovalue_crossreg, path(dir, "intovalue-crossreg.csv"))
