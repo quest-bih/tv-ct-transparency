5204bb7a5d6ee507143dd2091d754b5f4ae63fef
Maia Salholz-Hillel <mash2015@gmail.com>
2021-08-31 10:13:02 +0200

    Add query log

--- R/01_get-data.R
+++ R/01_get-data.R
@@ -5,6 +5,13 @@ library(fs)
 dir <- dir_create(here::here("data", "raw"))
 
 
+# QUERY LOGS --------------------------------------------------------------
+
+download.file(
+  "https://github.com/maia-sh/intovalue-data/raw/6b97fb62804aae1bab6c85bd2817baa027207bab/queries.log",
+  here::here("data", "queries.log")
+)
+
 # INTOVALUE ---------------------------------------------------------------
 
 intovalue <- read_rds("https://github.com/maia-sh/intovalue-data/blob/main/data/processed/trials.rds?raw=true")
