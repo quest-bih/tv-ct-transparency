8c701b47c6027da189b1b367fa8c84dec948d570
Maia Salholz-Hillel <mash2015@gmail.com>
2021-08-31 09:43:02 +0200

    Update to new intovalue-data, as csv, and new has_publication

--- R/02_create-tv-sample.R
+++ R/02_create-tv-sample.R
@@ -3,7 +3,7 @@ library(stringr)
 library(readr)
 
 intovalue <- read_csv(here::here("data", "raw", "intovalue.csv"))
-crossreg <- read_csv(here::here("data", "raw", "intovalue-crossreg.csv"))
+intovalue_crossreg <- read_csv(here::here("data", "raw", "intovalue-crossreg.csv"))
 
 dir <- fs::dir_create(here::here("data", "processed"))
 
@@ -13,16 +13,19 @@ trackvalue <-
 
   # Limit the trials to IV 2 from Charité
   filter(str_detect(lead_cities, "Berlin") & iv_version == 2) %>%
-  # Reapply IV inclusion criteria
-  filter(iv_completion, iv_status, iv_interventional)
 
-readr::write_rds(trackvalue, fs::path(dir, "trackvalue.rds"))
+  # Reapply IV inclusion criteria
+  filter(iv_completion, iv_status, iv_interventional) %>%
+
+  # # Trials with a journal article have a publication (disregard dissertations and abstracts)
+  mutate(has_publication = if_else(publication_type == "journal publication", TRUE, FALSE, missing = FALSE))
+
 readr::write_csv(trackvalue, fs::path(dir, "trackvalue.csv"))
 
 # Limit to cross-registrations of trials included in trackvalue
 trackvalue_crossreg <-
-  crossreg %>%
+  intovalue_crossreg %>%
   semi_join(trackvalue, by = c("id", "pmid", "doi")) %>%
   distinct()
 
-readr::write_rds(trackvalue_crossreg, fs::path(dir, "crossreg.rds"))
+readr::write_csv(trackvalue_crossreg, fs::path(dir, "crossreg.csv"))
