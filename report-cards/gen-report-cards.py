d731433adf606c3166be69a6266c163d4af16583
Delwen Franzen <delwen@franzen.fm>
2021-09-29 18:10:30 +0200

    Add first version tree for action layers
    - Action layers specify where improvement can
    be made on this trial
    - SVG layers not yet generated
    - TODO: include layer for publication of
    trial results (with reminder to include TRN)

--- report-cards/gen-report-cards.py
+++ report-cards/gen-report-cards.py
@@ -209,6 +209,66 @@ TABLE = {
             True: {"layer": "registration_layer_1"},
             False: {"layer": "registration_layer_2"}
         }
+    # },
+    # "#action": {
+    #     "has_summary_results": {
+    #         True: {
+    #             "has_reg_pub_link": {
+    #                 True: {
+    #                     "is_oa": {
+    #                         True: {"layer": "action_layer_1"},
+    #                         False: {
+    #                             "is_closed_archivable": {
+    #                                 True: {"layer": "action_layer_6"},
+    #                                 False: {"layer": "action_layer_1"},
+    #                                 np.NaN: {"layer": "action_layer_1"}
+    #                             }
+    #                         }
+    #                     }
+    #                 },
+    #                 False: {
+    #                     "is_oa": {
+    #                         True: {"layer": "action_layer_7"},
+    #                         False: {
+    #                             "is_closed_archivable": {
+    #                                 True: {"layer": "action_layer_3"},
+    #                                 False: {"layer": "action_layer_7"},
+    #                                 np.NaN: {"layer": "action_layer_7"}
+    #                             }
+    #                         }
+    #                     }
+    #                 }
+    #             }
+    #         },
+    #         False: {
+    #             "has_reg_pub_link": {
+    #                 True: {
+    #                     "is_oa": {
+    #                         True: {"layer": "action_layer_8"},
+    #                         False: {
+    #                             "is_closed_archivable": {
+    #                                 True: {"layer": "action_layer_4"},
+    #                                 False: {"layer": "action_layer_8"},
+    #                                 np.NaN: {"layer": "action_layer_8"}
+    #                             }
+    #                         }
+    #                     }
+    #                 },
+    #                 False: {
+    #                     "is_oa": {
+    #                         True: {"layer": "action_layer_5"},
+    #                         False: {
+    #                             "is_closed_archivable": {
+    #                                 True: {"layer": "action_layer_2"},
+    #                                 False: {"layer": "action_layer_5"},
+    #                                 np.NaN: {"layer": "action_layer_5"}
+    #                             }
+    #                         }
+    #                     }
+    #                 }
+    #             }
+    #         }
+    #     }
     }
 }
 
