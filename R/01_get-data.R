1d4e238fb7966c584469d33a5c75b29b8bc1b7c0
Delwen Franzen <delwen@franzen.fm>
2022-04-14 04:05:44 +0200

    Download data from updated intovalue-data branch
    - Get more recent registry and OA data from the update-2022-april branch of the intovalue-data repo

--- R/01_get-data.R
+++ R/01_get-data.R
@@ -8,19 +8,19 @@ dir <- dir_create(here::here("data", "raw"))
 # QUERY LOGS --------------------------------------------------------------
 
 download.file(
-  "https://raw.githubusercontent.com/maia-sh/intovalue-data/main/queries.log",
+  "https://raw.githubusercontent.com/maia-sh/intovalue-data/update-2022-april/queries.log",
   here::here("data", "queries.log")
 )
 
 # INTOVALUE ---------------------------------------------------------------
 
-intovalue <- read_rds("https://github.com/maia-sh/intovalue-data/blob/main/data/processed/trials.rds?raw=true")
+intovalue <- read_rds("https://github.com/maia-sh/intovalue-data/blob/update-2022-april/data/processed/trials.rds?raw=true")
 
 write_csv(intovalue, path(dir, "intovalue.csv"))
 
 # CROSS-REGISTRATIONS -----------------------------------------------------
 
 intovalue_crossreg <-
-  read_rds("https://github.com/maia-sh/intovalue-data/blob/main/data/processed/trn/cross-registrations.rds?raw=true")
+  read_rds("https://github.com/maia-sh/intovalue-data/blob/update-2022-april/data/processed/trn/cross-registrations.rds?raw=true")
 
 write_csv(intovalue_crossreg, path(dir, "intovalue-crossreg.csv"))
