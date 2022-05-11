56c585d822cbcab58c1162a7d42db41aa3725c24
Delwen Franzen <delwen@franzen.fm>
2022-05-11 19:30:55 +0200

    Update script to create email parameters

--- R/add-email-parameters.R
+++ R/add-email-parameters.R
@@ -5,15 +5,15 @@ library(stringr)
 data <- read_csv("data/processed/charite-contacts-per-trialist.csv")
 
 email_parameters <- data %>%
-  mutate(name_for_file = gsub("\\.", "", name)) %>%
-  mutate(name_for_file = str_replace_all(name_for_file, "ä", "ae")) %>%
-  mutate(name_for_file = str_replace_all(name_for_file, "ö", "oe")) %>%
-  mutate(name_for_file = str_replace_all(name_for_file, "ü", "ue")) %>%
-  mutate(name_for_file = tolower(name_for_file)) %>%
-  mutate(name_for_file = str_replace_all(name_for_file, " ", "-")) %>%
-  mutate(email_body_html = name_for_file) %>%
-  mutate(email_body_html = paste0("html/", email_body_html, ".html")) %>%
-  mutate(email_attachment = name_for_file) %>%
-  mutate(email_attachment = paste0("attach/", email_attachment, "-materials.pdf"))
+  mutate(
+    name_for_file = gsub("\\.", "", name),
+    name_for_file = str_replace_all(name_for_file, "ä", "ae"),
+    name_for_file = str_replace_all(name_for_file, "ö", "oe"),
+    name_for_file = str_replace_all(name_for_file, "ü", "ue"),
+    name_for_file = tolower(name_for_file),
+    name_for_file = str_replace_all(name_for_file, " ", "-"),
+    email_body_html = paste0("html/", name_for_file, ".html"),
+    email_attachment = paste0("attach/", name_for_file, "-materials.pdf")
+  )
 
-write_csv(email_parameters, "email_parameters.csv")
+write_csv(email_parameters, "data/processed/email_parameters.csv")
