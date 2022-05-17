114c5c4073106b51619b085e9ea70275b5013d12
Delwen Franzen <delwen@franzen.fm>
2022-05-17 17:31:06 +0200

    Add/update email parameters
    - Removed "-materials" from the attachment file name
    - Added the subject message for the initial email

--- R/add-email-parameters.R
+++ R/add-email-parameters.R
@@ -13,7 +13,8 @@ email_parameters <- data %>%
     name_for_file = tolower(name_for_file),
     name_for_file = str_replace_all(name_for_file, " ", "-"),
     email_body_html = paste0("html/", name_for_file, ".html"),
-    email_attachment = paste0("attach/", name_for_file, "-materials.pdf")
+    email_attachment = paste0("attach/", name_for_file, ".pdf"),
+    subject_message = "Pilotstudie zur Transparenz Ihrer klinischen Studie(n)"
   )
 
 write_csv(email_parameters, "data/processed/email_parameters.csv")
