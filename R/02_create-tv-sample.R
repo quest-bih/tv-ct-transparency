6fedf9ee4f2ff7d6ba679128ab61f02a55f3354d
Maia Salholz-Hillel <mash2015@gmail.com>
2022-05-21 19:05:58 +0200

    Update tv to account for changes in iv, also manually select iv1/2 dupe

--- R/02_create-tv-sample.R
+++ R/02_create-tv-sample.R
@@ -14,28 +14,57 @@ dir <- fs::dir_create(here::here("data", "processed"))
 trackvalue <-
   intovalue %>%
 
-  # Limit the trials to IV 2 from Charité
-  filter(str_detect(lead_cities, "Berlin") & iv_version == 2) %>%
+  # Limit to trials from Charité
+  filter(str_detect(lead_cities, "Berlin")) %>%
 
   # Reapply IV inclusion criteria
   filter(iv_completion, iv_status, iv_interventional) %>%
 
-  # Abstracts miscoded as journal article
-  # Could also change in `intovalue-data`
-  mutate(publication_type = if_else(id %in% c("NCT01600573", "NCT02052960", "NCT01770080", "NCT01181401"), "abstract", publication_type)) %>%
+  # For trials included in both IV 1 & 2, select which to keep
 
-  # DOI/PMID missing for one journal article (letter, no abstract)
-  # Also has a cross-registration: 2013-002319-82
-  # Could also change in `intovalue-data`: filter(intovalue, publication_type == "journal publication" & !is.na(url) & is.na(doi))
+  # First, limit to trials which were included in IV2 and put IV2 on top
+  group_by(id) %>%
+  filter(any(iv_version == 2)) %>%
+  arrange(desc(iv_version)) %>%
+
+  # If same publication for both IV 1 & 2, keep IV2 version
+  distinct(id, doi, pmid, .keep_all = TRUE) %>%
+  ungroup() %>%
+
+  # If different publications for IV 1 & 2, manually remove a version
+  # https://docs.google.com/spreadsheets/d/1RufE5pZL5PlBWx4SOryGbZ_DZxv2d5xuBQ6vcE4EDWk/edit#gid=288586668
+  filter(
+    !(id == "NCT00850629" & iv_version == 1),
+    !(id == "NCT01049100" & iv_version == 1),
+    !(id == "NCT01105143" & iv_version == 1),
+    !(id == "NCT01181401" & iv_version == 1),
+    !(id == "NCT01605487" & iv_version == 2)
+  ) %>%
+
+  # Check that only one row per id, and remove `iv_version`
+  assert(is_uniq, id) %>%
+  select(-iv_version) %>%
+
+  # Update ft status for trial with ft not currently parsed in intovalue-data
+  # Checked for crossreg in ft -> 2013-002319-82 TODO: add to crossreg
+  # TODO: update in intovalue-data with bcg and grobid
   rows_update(tibble(
     id = "NCT01984788",
-    doi = "10.1016/j.jaci.2016.03.043",
-    pmid = 27302552,
-    has_iv_trn_abstract = FALSE,
-    has_iv_trn_ft = TRUE,
-    has_reg_pub_link = FALSE
+    has_ft = TRUE,
+    ft_source = "doi",
+    has_iv_trn_ft = FALSE
   ), by = "id") %>%
 
+  # Checked for crossreg in ft -> none
+  rows_update(tibble(
+    id = "NCT01503372",
+    has_ft = TRUE,
+    ft_source = "doi",
+    has_iv_trn_ft = TRUE # in ethics statement
+  ), by = "id") %>%
+
+  # Add link info for trial with publication with doi and no pmid: trackvalue %>% filter(publication_type == "journal publication") %>% filter(is.na(pmid) & !is.na(doi))
+  # Checked for crossreg in ft -> none
   rows_update(tibble(
     id = "DRKS00004858",
     has_iv_trn_abstract = TRUE,
@@ -43,10 +72,7 @@ trackvalue <-
     has_reg_pub_link = FALSE
   ), by = "id") %>%
 
-  # Trials with a journal article have a publication (disregard dissertations and abstracts)
-  mutate(has_publication = if_else(publication_type == "journal publication", TRUE, FALSE, missing = FALSE)) %>%
-
-  # Add variable to record if days_reg_to_start is positive or negative
+  # Add variable to record if days_reg_to_start is positive or negative (for report card)
   mutate(days_reg_to_start_is_positive = if_else(days_reg_to_start > 0, TRUE, FALSE))
 
 # Check assumptions
@@ -65,7 +91,7 @@ trackvalue %>%
          has_reg_pub_link
   )
 
-# NOTE: some trials with non-journal article publication have data for publication metrics; could convert to NA or simply disregard via filtering
+# NOTE: some trials with non-journal article publication have data for publication metrics --> disregarded in report cards via filtering for `has_publication`
 trackvalue %>%
   count(!has_publication & is_publication_2y)
 
