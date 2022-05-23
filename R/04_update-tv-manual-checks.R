d94ac5085c042046192fe5fb038caeccee2180d0
Delwen Franzen <delwen@franzen.fm>
2022-05-23 13:30:38 +0200

    Add OA manual checks

--- R/04_update-tv-manual-checks.R
+++ R/04_update-tv-manual-checks.R
@@ -14,8 +14,51 @@ trackvalue_checked <-
   select(-starts_with("has_crossreg"), -starts_with("n_crossreg")) %>%
 
   # Add in euctr crossreg data
-  left_join(euctr_crossreg, by = "id")
+  left_join(euctr_crossreg, by = "id") %>%
 
-# TODO: delwen add oa
+  # accessible version of publication found on journal website
+  rows_update(tibble(
+    doi = "10.1016/j.jaci.2016.03.043",
+    color = "open-unclear",
+    color_green_only = "open-unclear",
+    is_oa = TRUE,
+    is_closed_archivable = NA
+    ), by = "doi") %>%
+
+  # accessible version of publication found on journal website
+  rows_update(tibble(
+    doi = "10.1097/eja.0000000000000929",
+    color = "open-unclear",
+    color_green_only = "open-unclear",
+    is_oa = TRUE,
+    is_closed_archivable = NA
+    ), by = "doi") %>%
+
+  # accessible version of publication found in Refubium
+  rows_update(tibble(
+    doi = "10.1017/s0033291716001379",
+    color = "green",
+    color_green_only = "green",
+    is_oa = TRUE,
+    is_closed_archivable = NA
+    ), by = "doi") %>%
+
+  # accessible version of publication found on journal website
+  rows_update(tibble(
+    doi = "10.1016/j.jaci.2012.06.047",
+    color = "open-unclear",
+    color_green_only = "open-unclear",
+    is_oa = TRUE,
+    is_closed_archivable = NA
+    ), by = "doi") %>%
+
+  # accessible version of publication found on journal website
+  rows_update(tibble(
+    doi = "10.1200/jco.2011.41.1553",
+    color = "open-unclear",
+    color_green_only = "open-unclear",
+    is_oa = TRUE,
+    is_closed_archivable = NA
+    ), by = "doi")
 
 write_csv(trackvalue_checked, here("data", "processed", "trackvalue-checked.csv"))
