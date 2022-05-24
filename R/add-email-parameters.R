9f2ae31f678b2b409b31d6c655bbc5ae9478e489
Delwen Franzen <delwen@franzen.fm>
2022-05-24 13:54:13 +0200

    Add folders for emails 0-4
    - Email 0: launch
    - [Email 1 is the non-personalized email from CSC]
    - Email 2: reminder 2
    - Email 3: reminder 3
    - Email 4: reminder 4

--- R/add-email-parameters.R
+++ R/add-email-parameters.R
@@ -12,7 +12,10 @@ email_parameters <- data %>%
     name_for_file = str_replace_all(name_for_file, "ü", "ue"),
     name_for_file = tolower(name_for_file),
     name_for_file = str_replace_all(name_for_file, " ", "-"),
-    email_body_html = paste0("html/", name_for_file, ".html"),
+    launch_email_html = paste0("0_email/", name_for_file, ".html"),
+    reminder_email_2_html = paste0("2_email/", name_for_file, ".html"),
+    reminder_email_3_html = paste0("3_email/", name_for_file, ".html"),
+    reminder_email_4_html = paste0("4_email/", name_for_file, ".html"),
     email_attachment = paste0("attach/", name_for_file, ".pdf"),
     subject_message = "Pilotstudie zur Transparenz Ihrer klinischen Studie(n)"
   )
